7dceea1da7359af0a6f60d3b23f009cc46bd8c73
ahmad <ahmadzainulqomar@mail.ugm.ac.id>
2026-03-25 04:35:04 +0700

    perbaikan

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,4 +3,4 @@ const SUPABASE_URL = "https://tgibueuvvefnhtceqrnl.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnaWJ1ZXV2dmVmbmh0Y2Vxcm5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNjc0NzEsImV4cCI6MjA4OTk0MzQ3MX0.mswoNtmfMxXzeevK-b-F7tGzVS1pSGQWDfXWVhf_usk";
 
 // Key LocalStorage lama bisa tetap ada atau dihapus nanti
-const LS_KEY_TOKEN = "gabku_app_token";
+const LS_KEY_TOKEN = "jendralgabku";
