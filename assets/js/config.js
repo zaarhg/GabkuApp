41ee0e16b79571053ee9142aaf3e9806fa54b156
ahmad <ahmadzainulqomar@mail.ugm.ac.id>
2026-03-25 04:41:34 +0700

    perbaikan

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,5 +2,8 @@
 const SUPABASE_URL = "https://tgibueuvvefnhtceqrnl.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnaWJ1ZXV2dmVmbmh0Y2Vxcm5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNjc0NzEsImV4cCI6MjA4OTk0MzQ3MX0.mswoNtmfMxXzeevK-b-F7tGzVS1pSGQWDfXWVhf_usk";
 
-// Key LocalStorage lama bisa tetap ada atau dihapus nanti
+// SCRIPT_URL (Gunakan nama ini agar sesuai dengan yang dipanggil di api.js)
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxhOGtUzUSU6gnwrEpOT-HdzErUASXJCAlJx8tHiGwSg7bw-zcNtKJrpt82dWuteEmn/exec";
+
+// Key Token (Pastikan sama dengan ACCESS_TOKEN di Script Properties GAS)
 const LS_KEY_TOKEN = "jendralgabku";
